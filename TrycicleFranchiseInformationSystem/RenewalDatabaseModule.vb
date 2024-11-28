50ea988701453e0fe3379989c9bbafaa5e503c6b
rubyruivivar <jeanneruivivar@gmail.com>
2024-11-29 01:52:13 +0800

    renewal: add, edit, delete, and displaying the database from xammp

---
+++ TrycicleFranchiseInformationSystem/RenewalDatabaseModule.vb
@@ -0,0 +1,35 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module RenewalDatabaseModule
+    Private connectionString As String = "Server=localhost;Database=student_profile;Uid=root;Pwd=;"
+    Private connection As New MySqlConnection(connectionString)
+
+    Public Sub ExecuteNonQuery(query As String)
+        Try
+            connection.Open()
+            Dim command As New MySqlCommand(query, connection)
+            command.ExecuteNonQuery()
+        Catch ex As Exception
+            Throw New Exception("Renewal Database error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    Public Function LoadToDGV(query As String, dgv As DataGridView) As Integer
+        Dim count As Integer = 0
+        Try
+            connection.Open()
+            Dim adapter As New MySqlDataAdapter(query, connection)
+            Dim dataTable As New DataTable()
+            adapter.Fill(dataTable)
+            dgv.DataSource = dataTable
+            count = dataTable.Rows.Count
+        Catch ex As Exception
+            Throw New Exception("Renewal Database error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+        Return count
+    End Function
+End Module
--- TrycicleFranchiseInformationSystem/UserControl5.Designer.vb
+++ TrycicleFranchiseInformationSystem/UserControl5.Designer.vb
@@ -23,22 +23,20 @@ Partial Class Form5Renewal
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         LabelRenewal = New Label()
-        tbrenewal_id = New TextBox()
-        tbfranchise_id = New TextBox()
-        tbsubmission_date = New TextBox()
-        tb_status = New TextBox()
-        tb_processing_date = New TextBox()
-        tb_approval_date = New TextBox()
-        tbfranchisee_id = New TextBox()
-        tb_fee_paid = New TextBox()
-        tb_expiration_date = New TextBox()
-        tb_renewal_type = New TextBox()
-        tb_compliance_stat = New TextBox()
-        tb_renewalfee = New TextBox()
-        tb_penaltyfee = New TextBox()
-        tb_authority_id = New TextBox()
-        tb_renewal_num = New TextBox()
-        BtnAdd = New Button()
+        cbxStatus = New ComboBox()
+        cbxComplianceStatus = New ComboBox()
+        cbxRenewalType = New ComboBox()
+        txtRenewalNumber = New TextBox()
+        txtAuthorityID = New TextBox()
+        txtPenaltyFee = New TextBox()
+        txtRenewalFee = New TextBox()
+        txtFranchiseeID = New TextBox()
+        txtFranchiseID = New TextBox()
+        btnSave = New Button()
+        dtpExpirationDate = New DateTimePicker()
+        dtpApprovalDate = New DateTimePicker()
+        dtpProcessingDate = New DateTimePicker()
+        dtpSubmissionDate = New DateTimePicker()
         SuspendLayout()
         ' 
         ' LabelRenewal
@@ -46,191 +44,180 @@ Partial Class Form5Renewal
         LabelRenewal.AutoSize = True
         LabelRenewal.FlatStyle = FlatStyle.Flat
         LabelRenewal.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        LabelRenewal.Location = New Point(222, 10)
+        LabelRenewal.Location = New Point(194, 8)
         LabelRenewal.Name = "LabelRenewal"
-        LabelRenewal.Size = New Size(253, 28)
+        LabelRenewal.Size = New Size(201, 21)
         LabelRenewal.TabIndex = 0
         LabelRenewal.Text = "RENEWAL INFORMATION"
         ' 
-        ' tbrenewal_id
+        ' cbxStatus
         ' 
-        tbrenewal_id.Location = New Point(29, 53)
-        tbrenewal_id.Name = "tbrenewal_id"
-        tbrenewal_id.PlaceholderText = "Enter Renewal ID"
-        tbrenewal_id.Size = New Size(248, 27)
-        tbrenewal_id.TabIndex = 1
+        cbxStatus.FormattingEnabled = True
+        cbxStatus.Items.AddRange(New Object() {"approved", "denied", "pending"})
+        cbxStatus.Location = New Point(304, 65)
+        cbxStatus.Name = "cbxStatus"
+        cbxStatus.Size = New Size(218, 23)
+        cbxStatus.TabIndex = 68
         ' 
-        ' tbfranchise_id
+        ' cbxComplianceStatus
         ' 
-        tbfranchise_id.Location = New Point(29, 86)
-        tbfranchise_id.Name = "tbfranchise_id"
-        tbfranchise_id.PlaceholderText = "Franchise ID"
-        tbfranchise_id.Size = New Size(248, 27)
-        tbfranchise_id.TabIndex = 2
+        cbxComplianceStatus.FormattingEnabled = True
+        cbxComplianceStatus.Items.AddRange(New Object() {"compliant", "non-compliant"})
+        cbxComplianceStatus.Location = New Point(304, 38)
+        cbxComplianceStatus.Name = "cbxComplianceStatus"
+        cbxComplianceStatus.Size = New Size(218, 23)
+        cbxComplianceStatus.TabIndex = 67
         ' 
-        ' tbsubmission_date
+        ' cbxRenewalType
         ' 
-        tbsubmission_date.Location = New Point(29, 119)
-        tbsubmission_date.Name = "tbsubmission_date"
-        tbsubmission_date.PlaceholderText = "Submission Date"
-        tbsubmission_date.Size = New Size(248, 27)
-        tbsubmission_date.TabIndex = 3
+        cbxRenewalType.FormattingEnabled = True
+        cbxRenewalType.Items.AddRange(New Object() {"annual", "semi-annual"})
+        cbxRenewalType.Location = New Point(59, 201)
+        cbxRenewalType.Name = "cbxRenewalType"
+        cbxRenewalType.Size = New Size(218, 23)
+        cbxRenewalType.TabIndex = 66
         ' 
-        ' tb_status
+        ' txtRenewalNumber
         ' 
-        tb_status.Location = New Point(29, 152)
-        tb_status.Name = "tb_status"
-        tb_status.PlaceholderText = "Status"
-        tb_status.Size = New Size(248, 27)
-        tb_status.TabIndex = 4
+        txtRenewalNumber.Location = New Point(304, 93)
+        txtRenewalNumber.Margin = New Padding(3, 2, 3, 2)
+        txtRenewalNumber.Name = "txtRenewalNumber"
+        txtRenewalNumber.PlaceholderText = "Renewal Number"
+        txtRenewalNumber.Size = New Size(218, 23)
+        txtRenewalNumber.TabIndex = 61
         ' 
-        ' tb_processing_date
+        ' txtAuthorityID
         ' 
-        tb_processing_date.Location = New Point(29, 185)
-        tb_processing_date.Name = "tb_processing_date"
-        tb_processing_date.PlaceholderText = "Processing Date"
-        tb_processing_date.Size = New Size(248, 27)
-        tb_processing_date.TabIndex = 5
+        txtAuthorityID.Location = New Point(302, 177)
+        txtAuthorityID.Margin = New Padding(3, 2, 3, 2)
+        txtAuthorityID.Name = "txtAuthorityID"
+        txtAuthorityID.PlaceholderText = "Authority ID"
+        txtAuthorityID.Size = New Size(219, 23)
+        txtAuthorityID.TabIndex = 60
         ' 
-        ' tb_approval_date
+        ' txtPenaltyFee
         ' 
-        tb_approval_date.Location = New Point(29, 218)
-        tb_approval_date.Name = "tb_approval_date"
-        tb_approval_date.PlaceholderText = "Approval Date"
-        tb_approval_date.Size = New Size(248, 27)
-        tb_approval_date.TabIndex = 6
+        txtPenaltyFee.Location = New Point(303, 147)
+        txtPenaltyFee.Margin = New Padding(3, 2, 3, 2)
+        txtPenaltyFee.Name = "txtPenaltyFee"
+        txtPenaltyFee.PlaceholderText = "Penalty Fee"
+        txtPenaltyFee.Size = New Size(219, 23)
+        txtPenaltyFee.TabIndex = 59
         ' 
-        ' tbfranchisee_id
+        ' txtRenewalFee
         ' 
-        tbfranchisee_id.Location = New Point(29, 251)
-        tbfranchisee_id.Name = "tbfranchisee_id"
-        tbfranchisee_id.PlaceholderText = "Franchisee ID"
-        tbfranchisee_id.Size = New Size(248, 27)
-        tbfranchisee_id.TabIndex = 7
+        txtRenewalFee.Location = New Point(303, 120)
+        txtRenewalFee.Margin = New Padding(3, 2, 3, 2)
+        txtRenewalFee.Name = "txtRenewalFee"
+        txtRenewalFee.PlaceholderText = "Renewal Fee"
+        txtRenewalFee.Size = New Size(219, 23)
+        txtRenewalFee.TabIndex = 58
         ' 
-        ' tb_fee_paid
+        ' txtFranchiseeID
         ' 
-        tb_fee_paid.Location = New Point(29, 284)
-        tb_fee_paid.Name = "tb_fee_paid"
-        tb_fee_paid.PlaceholderText = "Fee Paid"
-        tb_fee_paid.Size = New Size(248, 27)
-        tb_fee_paid.TabIndex = 8
+        txtFranchiseeID.Location = New Point(59, 66)
+        txtFranchiseeID.Margin = New Padding(3, 2, 3, 2)
+        txtFranchiseeID.Name = "txtFranchiseeID"
+        txtFranchiseeID.PlaceholderText = "Franchisee ID"
+        txtFranchiseeID.Size = New Size(218, 23)
+        txtFranchiseeID.TabIndex = 57
         ' 
-        ' tb_expiration_date
+        ' txtFranchiseID
         ' 
-        tb_expiration_date.Location = New Point(29, 317)
-        tb_expiration_date.Name = "tb_expiration_date"
-        tb_expiration_date.PlaceholderText = "Expiration Date"
-        tb_expiration_date.Size = New Size(248, 27)
-        tb_expiration_date.TabIndex = 9
+        txtFranchiseID.Location = New Point(59, 39)
+        txtFranchiseID.Margin = New Padding(3, 2, 3, 2)
+        txtFranchiseID.Name = "txtFranchiseID"
+        txtFranchiseID.PlaceholderText = "Franchise ID"
+        txtFranchiseID.Size = New Size(218, 23)
+        txtFranchiseID.TabIndex = 56
         ' 
-        ' tb_renewal_type
+        ' btnSave
         ' 
-        tb_renewal_type.Location = New Point(390, 53)
-        tb_renewal_type.Name = "tb_renewal_type"
-        tb_renewal_type.PlaceholderText = "Renewal Type"
-        tb_renewal_type.Size = New Size(248, 27)
-        tb_renewal_type.TabIndex = 10
+        btnSave.BackColor = Color.CornflowerBlue
+        btnSave.FlatAppearance.BorderSize = 0
+        btnSave.FlatAppearance.MouseDownBackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btnSave.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btnSave.FlatStyle = FlatStyle.Flat
+        btnSave.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnSave.ForeColor = Color.White
+        btnSave.Location = New Point(396, 248)
+        btnSave.Margin = New Padding(3, 2, 3, 2)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(125, 28)
+        btnSave.TabIndex = 69
+        btnSave.Text = "Save"
+        btnSave.UseVisualStyleBackColor = False
         ' 
-        ' tb_compliance_stat
+        ' dtpExpirationDate
         ' 
-        tb_compliance_stat.Location = New Point(390, 86)
-        tb_compliance_stat.Name = "tb_compliance_stat"
-        tb_compliance_stat.PlaceholderText = "Compliance Status"
-        tb_compliance_stat.Size = New Size(248, 27)
-        tb_compliance_stat.TabIndex = 11
+        dtpExpirationDate.Location = New Point(59, 174)
+        dtpExpirationDate.Name = "dtpExpirationDate"
+        dtpExpirationDate.Size = New Size(218, 23)
+        dtpExpirationDate.TabIndex = 82
         ' 
-        ' tb_renewalfee
+        ' dtpApprovalDate
         ' 
-        tb_renewalfee.Location = New Point(390, 119)
-        tb_renewalfee.Name = "tb_renewalfee"
-        tb_renewalfee.PlaceholderText = "Renewal Fee"
-        tb_renewalfee.Size = New Size(248, 27)
-        tb_renewalfee.TabIndex = 12
+        dtpApprovalDate.Location = New Point(59, 147)
+        dtpApprovalDate.Name = "dtpApprovalDate"
+        dtpApprovalDate.Size = New Size(218, 23)
+        dtpApprovalDate.TabIndex = 81
         ' 
-        ' tb_penaltyfee
+        ' dtpProcessingDate
         ' 
-        tb_penaltyfee.Location = New Point(390, 152)
-        tb_penaltyfee.Name = "tb_penaltyfee"
-        tb_penaltyfee.PlaceholderText = "Penalty Fee"
-        tb_penaltyfee.Size = New Size(248, 27)
-        tb_penaltyfee.TabIndex = 13
+        dtpProcessingDate.Location = New Point(59, 120)
+        dtpProcessingDate.Name = "dtpProcessingDate"
+        dtpProcessingDate.Size = New Size(218, 23)
+        dtpProcessingDate.TabIndex = 80
         ' 
-        ' tb_authority_id
+        ' dtpSubmissionDate
         ' 
-        tb_authority_id.Location = New Point(390, 185)
-        tb_authority_id.Name = "tb_authority_id"
-        tb_authority_id.PlaceholderText = "Authority ID"
-        tb_authority_id.Size = New Size(248, 27)
-        tb_authority_id.TabIndex = 14
-        ' 
-        ' tb_renewal_num
-        ' 
-        tb_renewal_num.Location = New Point(390, 218)
-        tb_renewal_num.Name = "tb_renewal_num"
-        tb_renewal_num.PlaceholderText = "Renewal Number"
-        tb_renewal_num.Size = New Size(248, 27)
-        tb_renewal_num.TabIndex = 15
-        ' 
-        ' BtnAdd
-        ' 
-        BtnAdd.BackColor = Color.DarkBlue
-        BtnAdd.FlatAppearance.BorderSize = 0
-        BtnAdd.FlatStyle = FlatStyle.Flat
-        BtnAdd.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        BtnAdd.ForeColor = Color.White
-        BtnAdd.Location = New Point(458, 267)
-        BtnAdd.Name = "BtnAdd"
-        BtnAdd.Size = New Size(100, 35)
-        BtnAdd.TabIndex = 16
-        BtnAdd.Text = "Add"
-        BtnAdd.UseVisualStyleBackColor = False
+        dtpSubmissionDate.Location = New Point(59, 93)
+        dtpSubmissionDate.Name = "dtpSubmissionDate"
+        dtpSubmissionDate.Size = New Size(218, 23)
+        dtpSubmissionDate.TabIndex = 79
         ' 
         ' Form5Renewal
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         AutoSize = True
         BackColor = Color.RosyBrown
-        Controls.Add(BtnAdd)
-        Controls.Add(tb_renewal_num)
-        Controls.Add(tb_authority_id)
-        Controls.Add(tb_penaltyfee)
-        Controls.Add(tb_renewalfee)
-        Controls.Add(tb_compliance_stat)
-        Controls.Add(tb_renewal_type)
-        Controls.Add(tb_expiration_date)
-        Controls.Add(tb_fee_paid)
-        Controls.Add(tbfranchisee_id)
-        Controls.Add(tb_approval_date)
-        Controls.Add(tb_processing_date)
-        Controls.Add(tb_status)
-        Controls.Add(tbsubmission_date)
-        Controls.Add(tbfranchise_id)
-        Controls.Add(tbrenewal_id)
+        Controls.Add(dtpExpirationDate)
+        Controls.Add(dtpApprovalDate)
+        Controls.Add(dtpProcessingDate)
+        Controls.Add(dtpSubmissionDate)
+        Controls.Add(btnSave)
+        Controls.Add(cbxStatus)
+        Controls.Add(cbxComplianceStatus)
+        Controls.Add(cbxRenewalType)
+        Controls.Add(txtRenewalNumber)
+        Controls.Add(txtAuthorityID)
+        Controls.Add(txtPenaltyFee)
+        Controls.Add(txtRenewalFee)
+        Controls.Add(txtFranchiseeID)
+        Controls.Add(txtFranchiseID)
         Controls.Add(LabelRenewal)
+        Margin = New Padding(3, 2, 3, 2)
         Name = "Form5Renewal"
-        Size = New Size(682, 534)
+        Size = New Size(594, 461)
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents LabelRenewal As Label
-    Friend WithEvents tbrenewal_id As TextBox
-    Friend WithEvents tbfranchise_id As TextBox
-    Friend WithEvents tbsubmission_date As TextBox
-    Friend WithEvents tb_status As TextBox
-    Friend WithEvents tb_processing_date As TextBox
-    Friend WithEvents tb_approval_date As TextBox
-    Friend WithEvents tbfranchisee_id As TextBox
-    Friend WithEvents tb_fee_paid As TextBox
-    Friend WithEvents tb_expiration_date As TextBox
-    Friend WithEvents tb_renewal_type As TextBox
-    Friend WithEvents tb_compliance_stat As TextBox
-    Friend WithEvents tb_renewalfee As TextBox
-    Friend WithEvents tb_penaltyfee As TextBox
-    Friend WithEvents tb_authority_id As TextBox
-    Friend WithEvents tb_renewal_num As TextBox
-    Friend WithEvents BtnAdd As Button
+    Friend WithEvents cbxStatus As ComboBox
+    Friend WithEvents cbxComplianceStatus As ComboBox
+    Friend WithEvents cbxRenewalType As ComboBox
+    Friend WithEvents txtRenewalNumber As TextBox
+    Friend WithEvents txtAuthorityID As TextBox
+    Friend WithEvents txtPenaltyFee As TextBox
+    Friend WithEvents txtRenewalFee As TextBox
+    Friend WithEvents txtFranchiseeID As TextBox
+    Friend WithEvents txtFranchiseID As TextBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents dtpExpirationDate As DateTimePicker
+    Friend WithEvents dtpApprovalDate As DateTimePicker
+    Friend WithEvents dtpProcessingDate As DateTimePicker
+    Friend WithEvents dtpSubmissionDate As DateTimePicker
 
 End Class
