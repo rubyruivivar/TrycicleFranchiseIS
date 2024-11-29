a10aa04e615e5c2cfd68eaf45a8e9b62860c50d2
rubyruivivar <jeanneruivivar@gmail.com>
2024-11-29 22:26:51 +0800

    usercontrol12: renewal

---
+++ TrycicleFranchiseInformationSystem/UserControl12.Designer.vb
@@ -0,0 +1,269 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class RenewalData
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        dgvRenewal = New DataGridView()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        LabelRenewal = New Label()
+        lblRecordCount = New Label()
+        cbxStatus = New ComboBox()
+        cbxComplianceStatus = New ComboBox()
+        cbxRenewalType = New ComboBox()
+        dtpExpirationDate = New DateTimePicker()
+        dtpApprovalDate = New DateTimePicker()
+        dtpProcessingDate = New DateTimePicker()
+        dtpSubmissionDate = New DateTimePicker()
+        txtRenewalNumber = New TextBox()
+        txtAuthorityID = New TextBox()
+        txtPenaltyFee = New TextBox()
+        txtRenewalFee = New TextBox()
+        txtFranchiseID = New TextBox()
+        txtFranchiseeID = New TextBox()
+        CType(dgvRenewal, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' dgvRenewal
+        ' 
+        dgvRenewal.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        dgvRenewal.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+        dgvRenewal.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvRenewal.Location = New Point(32, 356)
+        dgvRenewal.Name = "dgvRenewal"
+        dgvRenewal.Size = New Size(893, 298)
+        dgvRenewal.TabIndex = 0
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.BackColor = Color.CornflowerBlue
+        btnUpdate.FlatAppearance.BorderSize = 0
+        btnUpdate.FlatAppearance.MouseDownBackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btnUpdate.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btnUpdate.FlatStyle = FlatStyle.Flat
+        btnUpdate.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnUpdate.ForeColor = Color.White
+        btnUpdate.Location = New Point(134, 275)
+        btnUpdate.Margin = New Padding(3, 2, 3, 2)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(125, 28)
+        btnUpdate.TabIndex = 30
+        btnUpdate.Text = "Update"
+        btnUpdate.UseVisualStyleBackColor = False
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.BackColor = Color.CornflowerBlue
+        btnDelete.FlatAppearance.BorderSize = 0
+        btnDelete.FlatAppearance.MouseDownBackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btnDelete.FlatAppearance.MouseOverBackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btnDelete.FlatStyle = FlatStyle.Flat
+        btnDelete.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnDelete.ForeColor = Color.White
+        btnDelete.Location = New Point(305, 275)
+        btnDelete.Margin = New Padding(3, 2, 3, 2)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(125, 28)
+        btnDelete.TabIndex = 31
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = False
+        ' 
+        ' LabelRenewal
+        ' 
+        LabelRenewal.AutoSize = True
+        LabelRenewal.FlatStyle = FlatStyle.Flat
+        LabelRenewal.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LabelRenewal.Location = New Point(31, 13)
+        LabelRenewal.Name = "LabelRenewal"
+        LabelRenewal.Size = New Size(201, 21)
+        LabelRenewal.TabIndex = 32
+        LabelRenewal.Text = "RENEWAL INFORMATION"
+        ' 
+        ' lblRecordCount
+        ' 
+        lblRecordCount.AutoSize = True
+        lblRecordCount.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblRecordCount.Location = New Point(32, 338)
+        lblRecordCount.Name = "lblRecordCount"
+        lblRecordCount.Size = New Size(43, 15)
+        lblRecordCount.TabIndex = 48
+        lblRecordCount.Text = "Label2"
+        ' 
+        ' cbxStatus
+        ' 
+        cbxStatus.FormattingEnabled = True
+        cbxStatus.Items.AddRange(New Object() {"approved", "denied", "pending"})
+        cbxStatus.Location = New Point(276, 98)
+        cbxStatus.Name = "cbxStatus"
+        cbxStatus.Size = New Size(218, 23)
+        cbxStatus.TabIndex = 81
+        ' 
+        ' cbxComplianceStatus
+        ' 
+        cbxComplianceStatus.FormattingEnabled = True
+        cbxComplianceStatus.Items.AddRange(New Object() {"compliant", "non-compliant"})
+        cbxComplianceStatus.Location = New Point(276, 71)
+        cbxComplianceStatus.Name = "cbxComplianceStatus"
+        cbxComplianceStatus.Size = New Size(218, 23)
+        cbxComplianceStatus.TabIndex = 80
+        ' 
+        ' cbxRenewalType
+        ' 
+        cbxRenewalType.FormattingEnabled = True
+        cbxRenewalType.Items.AddRange(New Object() {"annual", "semi-annual"})
+        cbxRenewalType.Location = New Point(31, 233)
+        cbxRenewalType.Name = "cbxRenewalType"
+        cbxRenewalType.Size = New Size(218, 23)
+        cbxRenewalType.TabIndex = 79
+        ' 
+        ' dtpExpirationDate
+        ' 
+        dtpExpirationDate.Location = New Point(31, 207)
+        dtpExpirationDate.Name = "dtpExpirationDate"
+        dtpExpirationDate.Size = New Size(218, 23)
+        dtpExpirationDate.TabIndex = 78
+        ' 
+        ' dtpApprovalDate
+        ' 
+        dtpApprovalDate.Location = New Point(31, 180)
+        dtpApprovalDate.Name = "dtpApprovalDate"
+        dtpApprovalDate.Size = New Size(218, 23)
+        dtpApprovalDate.TabIndex = 77
+        ' 
+        ' dtpProcessingDate
+        ' 
+        dtpProcessingDate.Location = New Point(31, 153)
+        dtpProcessingDate.Name = "dtpProcessingDate"
+        dtpProcessingDate.Size = New Size(218, 23)
+        dtpProcessingDate.TabIndex = 76
+        ' 
+        ' dtpSubmissionDate
+        ' 
+        dtpSubmissionDate.Location = New Point(31, 126)
+        dtpSubmissionDate.Name = "dtpSubmissionDate"
+        dtpSubmissionDate.Size = New Size(218, 23)
+        dtpSubmissionDate.TabIndex = 75
+        ' 
+        ' txtRenewalNumber
+        ' 
+        txtRenewalNumber.Location = New Point(276, 126)
+        txtRenewalNumber.Margin = New Padding(3, 2, 3, 2)
+        txtRenewalNumber.Name = "txtRenewalNumber"
+        txtRenewalNumber.PlaceholderText = "Renewal Number"
+        txtRenewalNumber.Size = New Size(218, 23)
+        txtRenewalNumber.TabIndex = 74
+        ' 
+        ' txtAuthorityID
+        ' 
+        txtAuthorityID.Location = New Point(275, 207)
+        txtAuthorityID.Margin = New Padding(3, 2, 3, 2)
+        txtAuthorityID.Name = "txtAuthorityID"
+        txtAuthorityID.PlaceholderText = "Authority ID"
+        txtAuthorityID.Size = New Size(219, 23)
+        txtAuthorityID.TabIndex = 73
+        ' 
+        ' txtPenaltyFee
+        ' 
+        txtPenaltyFee.Location = New Point(275, 180)
+        txtPenaltyFee.Margin = New Padding(3, 2, 3, 2)
+        txtPenaltyFee.Name = "txtPenaltyFee"
+        txtPenaltyFee.PlaceholderText = "Penalty Fee"
+        txtPenaltyFee.Size = New Size(219, 23)
+        txtPenaltyFee.TabIndex = 72
+        ' 
+        ' txtRenewalFee
+        ' 
+        txtRenewalFee.Location = New Point(275, 153)
+        txtRenewalFee.Margin = New Padding(3, 2, 3, 2)
+        txtRenewalFee.Name = "txtRenewalFee"
+        txtRenewalFee.PlaceholderText = "Renewal Fee"
+        txtRenewalFee.Size = New Size(219, 23)
+        txtRenewalFee.TabIndex = 71
+        ' 
+        ' txtFranchiseID
+        ' 
+        txtFranchiseID.Location = New Point(31, 71)
+        txtFranchiseID.Margin = New Padding(3, 2, 3, 2)
+        txtFranchiseID.Name = "txtFranchiseID"
+        txtFranchiseID.PlaceholderText = "Franchisee ID"
+        txtFranchiseID.Size = New Size(218, 23)
+        txtFranchiseID.TabIndex = 70
+        ' 
+        ' txtFranchiseeID
+        ' 
+        txtFranchiseeID.Location = New Point(31, 98)
+        txtFranchiseeID.Margin = New Padding(3, 2, 3, 2)
+        txtFranchiseeID.Name = "txtFranchiseeID"
+        txtFranchiseeID.PlaceholderText = "Franchise ID"
+        txtFranchiseeID.Size = New Size(218, 23)
+        txtFranchiseeID.TabIndex = 69
+        ' 
+        ' RenewalData
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.RosyBrown
+        Controls.Add(cbxStatus)
+        Controls.Add(cbxComplianceStatus)
+        Controls.Add(cbxRenewalType)
+        Controls.Add(dtpExpirationDate)
+        Controls.Add(dtpApprovalDate)
+        Controls.Add(dtpProcessingDate)
+        Controls.Add(dtpSubmissionDate)
+        Controls.Add(txtRenewalNumber)
+        Controls.Add(txtAuthorityID)
+        Controls.Add(txtPenaltyFee)
+        Controls.Add(txtRenewalFee)
+        Controls.Add(txtFranchiseID)
+        Controls.Add(txtFranchiseeID)
+        Controls.Add(lblRecordCount)
+        Controls.Add(LabelRenewal)
+        Controls.Add(btnDelete)
+        Controls.Add(btnUpdate)
+        Controls.Add(dgvRenewal)
+        Name = "RenewalData"
+        Size = New Size(949, 678)
+        CType(dgvRenewal, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents dgvRenewal As DataGridView
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents LabelRenewal As Label
+    Friend WithEvents lblRecordCount As Label
+    Friend WithEvents cbxStatus As ComboBox
+    Friend WithEvents cbxComplianceStatus As ComboBox
+    Friend WithEvents cbxRenewalType As ComboBox
+    Friend WithEvents dtpExpirationDate As DateTimePicker
+    Friend WithEvents dtpApprovalDate As DateTimePicker
+    Friend WithEvents dtpProcessingDate As DateTimePicker
+    Friend WithEvents dtpSubmissionDate As DateTimePicker
+    Friend WithEvents txtRenewalNumber As TextBox
+    Friend WithEvents txtAuthorityID As TextBox
+    Friend WithEvents txtPenaltyFee As TextBox
+    Friend WithEvents txtRenewalFee As TextBox
+    Friend WithEvents txtFranchiseID As TextBox
+    Friend WithEvents txtFranchiseeID As TextBox
+
+End Class
---
+++ TrycicleFranchiseInformationSystem/UserControl12.vb
@@ -0,0 +1,229 @@
+﻿Public Class RenewalData
+    Dim recordCount As Integer = 0
+
+    ' Refresh Renewal DataGridView
+    Private Sub RefreshRenewalData()
+        Try
+            Dim query As String = "SELECT `renewal_id` AS 'Renewal ID', `franchise_id` AS 'Franchise ID', `franchisee_id` AS 'Franchisee ID', " &
+                                  "`submission_date` AS 'Submission Date', `processing_date` AS 'Processing Date', `approval_date` AS 'Approval Date', " &
+                                  "`expiration_date` AS 'Expiration Date', `renewal_type` AS 'Renewal Type', `compliance_status` AS 'Compliance Status', " &
+                                  "`status` AS 'Status', `renewal_number` AS 'Renewal Number', `renewal_fee` AS 'Renewal Fee', `penalty_fee` AS 'Penalty Fee', " &
+                                  "`fee_paid` AS 'Fee Paid', `authority_id` AS 'Authority ID' FROM renewaldatabase"
+            recordCount = RenewalDatabaseModule.LoadToDGV(query, dgvRenewal)
+            lblRecordCount.Text = "Total Records: " & recordCount
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    ' Load Event
+    Private Sub RenewalData_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Dock = DockStyle.Fill
+
+        ' Set placeholders for all ComboBoxes
+        SetComboBoxPlaceholder(cbxRenewalType, "Renewal Type")
+        SetComboBoxPlaceholder(cbxComplianceStatus, "Compliance Status")
+        SetComboBoxPlaceholder(cbxStatus, "Status")
+
+        RefreshRenewalData()
+    End Sub
+
+    ' ComboBox Placeholder Logic
+    Private Sub ComboBox_GotFocus(sender As Object, e As EventArgs) _
+        Handles cbxRenewalType.GotFocus, cbxComplianceStatus.GotFocus, cbxStatus.GotFocus
+
+        Dim comboBox As ComboBox = CType(sender, ComboBox)
+
+        ' Clear placeholder text on focus
+        If comboBox.ForeColor = Color.Gray Then
+            comboBox.Text = ""
+            comboBox.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub ComboBox_LostFocus(sender As Object, e As EventArgs) _
+        Handles cbxRenewalType.LostFocus, cbxComplianceStatus.LostFocus, cbxStatus.LostFocus
+
+        Dim comboBox As ComboBox = CType(sender, ComboBox)
+
+        ' Restore placeholder text if no value is selected
+        If String.IsNullOrWhiteSpace(comboBox.Text) Then
+            Select Case comboBox.Name
+                Case "cbxRenewalType"
+                    SetComboBoxPlaceholder(comboBox, "Renewal Type")
+                Case "cbxComplianceStatus"
+                    SetComboBoxPlaceholder(comboBox, "Compliance Status")
+                Case "cbxStatus"
+                    SetComboBoxPlaceholder(comboBox, "Status")
+            End Select
+        End If
+    End Sub
+
+    Private Sub SetComboBoxPlaceholder(comboBox As ComboBox, placeholder As String)
+        comboBox.Text = placeholder
+        comboBox.ForeColor = Color.Gray
+    End Sub
+
+    ' Update Button Click
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Try
+            If txtFranchiseID.Tag Is Nothing Then
+                MsgBox("Select a record to update.", MsgBoxStyle.Exclamation)
+                Exit Sub
+            End If
+
+            ' Validate that the required fields are filled
+            If txtFranchiseID.Text.Trim = Nothing Then
+                MsgBox("Enter Franchisee ID.")
+                txtFranchiseID.Focus()
+                Exit Sub
+            End If
+            If txtFranchiseeID.Text.Trim = Nothing Then
+                MsgBox("Enter Franchisee ID.")
+                txtFranchiseeID.Focus()
+                Exit Sub
+            End If
+
+            ' Validation: Check if the required fields are not empty
+            If cbxRenewalType.Text = "Renewal Type" OrElse cbxComplianceStatus.Text = "Compliance Status" OrElse cbxStatus.Text = "Status" Then
+                MsgBox("Please fill in all required fields.", MsgBoxStyle.Exclamation)
+                Exit Sub
+            End If
+
+            If txtRenewalNumber.Text.Trim = Nothing Then
+                MsgBox("Enter Renewal Number.")
+                txtRenewalNumber.Focus()
+                Exit Sub
+            End If
+            If txtRenewalFee.Text.Trim = Nothing Then
+                MsgBox("Enter Renewal Fee.")
+                txtRenewalFee.Focus()
+                Exit Sub
+            End If
+            If txtPenaltyFee.Text.Trim = Nothing Then
+                MsgBox("Enter Penalty Fee.")
+                txtPenaltyFee.Focus()
+                Exit Sub
+            End If
+            If txtAuthorityID.Text.Trim = Nothing Then
+                MsgBox("Enter Fee Paid.")
+                txtAuthorityID.Focus()
+                Exit Sub
+            End If
+
+            ' Confirm update action
+            If MsgBox("Are you sure you want to update?", MsgBoxStyle.YesNo, "Update") = MsgBoxResult.Yes Then
+                ' Update query including all the fields except renewal_id (primary key)
+                Dim query As String = String.Format("UPDATE renewaldatabase SET " &
+                                                    "franchise_id='{0}', franchisee_id='{1}', submission_date='{2}', " &
+                                                    "processing_date='{3}', approval_date='{4}', expiration_date='{5}', " &
+                                                    "renewal_type='{6}', compliance_status='{7}', status='{8}', " &
+                                                    "renewal_number='{9}', renewal_fee='{10}', penalty_fee='{11}', " &
+                                                    "authority_id='{12}' " &
+                                                    "WHERE renewal_id='{13}'",
+                                                    txtFranchiseID.Text, txtFranchiseeID.Text, dtpSubmissionDate.Value.ToString("yyyy-MM-dd"),
+                                                    dtpProcessingDate.Value.ToString("yyyy-MM-dd"), dtpApprovalDate.Value.ToString("yyyy-MM-dd"),
+                                                    dtpExpirationDate.Value.ToString("yyyy-MM-dd"), cbxRenewalType.Text, cbxComplianceStatus.Text,
+                                                    cbxStatus.Text, txtRenewalNumber.Text, txtRenewalFee.Text, txtPenaltyFee.Text, txtAuthorityID.Text,
+                                                    txtFranchiseID.Tag)
+
+                ' Execute the query
+                ' RenewalDatabaseModule.ExecuteNonQuery(query)
+                modDB.readQuery(query)
+
+                ' Notify success
+                MsgBox("Record updated successfully!", MsgBoxStyle.Information)
+
+                ' Refresh the DataGridView
+                RefreshRenewalData()
+
+                ' Optionally, clear the form after saving
+                ClearFormFields()
+
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    ' Delete Button Click
+    ' Delete Button Click
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Try
+            ' Check if a record is selected
+            If txtFranchiseID.Tag Is Nothing Then
+                MsgBox("Select a record to delete.", MsgBoxStyle.Exclamation)
+                Exit Sub
+            End If
+
+            ' Confirmation message
+            If MsgBox("Are you sure you want to delete record with Renewal ID: " & txtFranchiseID.Tag &
+                  " belonging to Franchisee ID: " & txtFranchiseeID.Text & "?",
+                  MsgBoxStyle.YesNo, "Delete Record") = MsgBoxResult.Yes Then
+
+                ' Delete query
+                Dim query As String = String.Format("DELETE FROM renewaldatabase WHERE renewal_id='{0}'", txtFranchiseID.Tag)
+
+                ' Execute the query
+                ' RenewalDatabaseModule.ExecuteNonQuery(query)
+                modDB.readQuery(query)
+
+                ' Notify success
+                MsgBox("Record deleted successfully!", MsgBoxStyle.Information)
+
+                ' Refresh the DataGridView
+                RefreshRenewalData()
+
+                ' Optionally, clear the form after saving
+                ClearFormFields()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+
+    ' DataGridView CellDoubleClick Event
+    Private Sub dgvRenewal_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvRenewal.CellDoubleClick
+        Try
+            If e.RowIndex >= 0 Then
+                ' Set fields from selected row
+                txtFranchiseID.Tag = dgvRenewal.Rows(e.RowIndex).Cells(0).Value.ToString()
+                txtFranchiseID.Text = dgvRenewal.Rows(e.RowIndex).Cells(1).Value.ToString()
+                txtFranchiseeID.Text = dgvRenewal.Rows(e.RowIndex).Cells(2).Value.ToString()
+                dtpSubmissionDate.Value = DateTime.Parse(dgvRenewal.Rows(e.RowIndex).Cells(3).Value.ToString())
+                dtpProcessingDate.Value = DateTime.Parse(dgvRenewal.Rows(e.RowIndex).Cells(4).Value.ToString())
+                dtpApprovalDate.Value = DateTime.Parse(dgvRenewal.Rows(e.RowIndex).Cells(5).Value.ToString())
+                dtpExpirationDate.Value = DateTime.Parse(dgvRenewal.Rows(e.RowIndex).Cells(6).Value.ToString())
+                cbxRenewalType.Text = dgvRenewal.Rows(e.RowIndex).Cells(7).Value.ToString()
+                cbxComplianceStatus.Text = dgvRenewal.Rows(e.RowIndex).Cells(8).Value.ToString()
+                cbxStatus.Text = dgvRenewal.Rows(e.RowIndex).Cells(9).Value.ToString()
+                txtRenewalNumber.Text = dgvRenewal.Rows(e.RowIndex).Cells(10).Value.ToString()
+                txtRenewalFee.Text = dgvRenewal.Rows(e.RowIndex).Cells(11).Value.ToString()
+                txtPenaltyFee.Text = dgvRenewal.Rows(e.RowIndex).Cells(12).Value.ToString()
+                txtAuthorityID.Text = dgvRenewal.Rows(e.RowIndex).Cells(14).Value.ToString()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    ' Clear form fields after successful save
+    Private Sub ClearFormFields()
+        txtFranchiseID.Clear()
+        txtFranchiseeID.Clear()
+        txtRenewalNumber.Clear()
+        txtRenewalFee.Clear()
+        txtPenaltyFee.Clear()
+        txtAuthorityID.Clear()
+
+        cbxRenewalType.Text = "Renewal Type"
+        cbxComplianceStatus.Text = "Compliance Status"
+        cbxStatus.Text = "Status"
+        cbxRenewalType.ForeColor = Color.Gray
+        cbxComplianceStatus.ForeColor = Color.Gray
+        cbxStatus.ForeColor = Color.Gray
+
+    End Sub
+
+End Class
